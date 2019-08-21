c646df4b8b947cae2f70115e36957db4d0486d53
heykarthikwithu <heykarthikwithu@gmail.com>
2019-08-21 15:17:09 +0530

    blogs

--- blogs.sh
+++ blogs.sh
@@ -4,13 +4,32 @@ for row in $(echo "${sample}" | jq -r '.[] | @base64'); do
     _jq() {
      echo ${row} | base64 --decode | jq -r ${1}
     }
-   echo "<script>
+   echo "<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset='UTF-8'>
+	<title>$(_jq '.title') | KarthiK | Bangalore</title>
+	<meta name='description' content='$(_jq '.field_search_description')'>
+	<link rel='canonical' href='https://karthikkumardk.co.in/$(_jq '.view_node')' />
+	<meta name='robots' content='index, follow'>
+	<meta property='og:type' content='profile' />
+	<meta property='og:title' content='$(_jq '.title') | KarthiK | Bangalore' />
+	<meta property='og:description' content='$(_jq '.field_search_description')' />
+	<meta property='og:url' content='https://karthikkumardk.co.in/$(_jq '.view_node')' />
+   <script>
     window.onload = function() {
         var getUrl = window.location;
         var blogUrl = \"http://dev-karthikkumardk.pantheonsite.io/\" + getUrl.pathname.split('/')[1] +\"/\"+ getUrl.pathname.split('/')[2];
         window.location.href = blogUrl;
     }
-</script>" > $(echo $(_jq '.view_node').html | cut -c 2-)
+</script>
+</head>
+<body>
+<!-- Page Preloder -->
+	<div id='preloder'>
+		<div class='loader'></div>
+	</div>
+</body>" > $(echo $(_jq '.view_node').html | cut -c 2-)
 done
 
 echo "Robots.txt"
