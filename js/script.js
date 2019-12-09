6605404cb52eaf3e504b948374b3ef5a38c4e1f3
heykarthikwithu <heykarthikwithu@gmail.com>
2019-12-09 11:51:55 +0530

    css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(function(){
     // $("#intro-section").load("content/intro-section.html");
     $("#footer").load("content/footer.html");
+
+    $("#see-more").click(function(){
+        $("#search-results > div").css("display", "block").fadeIn(1500);
+        $("#see-more").css("display", "none");
+    });
 });
