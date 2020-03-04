3497c6a6376fd4d96d43c2a0b2e94ba778a160e5
heykarthikwithu <heykarthikwithu@gmail.com>
2020-03-04 17:32:58 +0530

    Product search

--- js/script.js
+++ js/script.js
@@ -6,4 +6,34 @@ $(function(){
         $("#search-results > div").css("display", "block").fadeIn(1500);
         $("#see-more").css("display", "none");
     });
+
+    $("#price-500").click(function () {
+        var price = getPriceV("price-500");
+        $('#search-results .price-500').css("display", "block");
+        $('#search-results .price-1000').css("display", "none");
+        $('#search-results .price-2000').css("display", "none");
+        $("#price-1000").prop("checked", false);
+        $("#price-2000").prop("checked", false);
+    });
+    $("#price-1000").click(function () {
+        var price = getPriceV("price-1000");
+        $('#search-results .price-500').css("display", "none");
+        $('#search-results .price-1000').css("display", "block");
+        $('#search-results .price-2000').css("display", "none");
+        $("#price-500").prop("checked", false);
+        $("#price-2000").prop("checked", false);
+    });
+    $("#price-2000").click(function () {
+        var price = getPriceV("price-2000");
+        $('#search-results .price-500').css("display", "none");
+        $('#search-results .price-1000').css("display", "none");
+        $('#search-results .price-2000').css("display", "block");
+        $("#price-500").prop("checked", false);
+        $("#price-1000").prop("checked", false);
+    });
 });
+
+function getPriceV(price_id) {
+    var price = price_id.split("-");
+    return price[1];
+}
