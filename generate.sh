681cfa77db16be0637fbb6c395972d2f26d9aab9
heykarthikwithu <heykarthikwithu@gmail.com>
2020-03-05 07:50:07 +0530

    comment product and common people

--- generate.sh
+++ generate.sh
@@ -105,7 +105,7 @@ Hey KarthiK,
             <a href='https://www.linkedin.com/in/karthikkumardk' target='_blank' title='Linkedin | Karthik Kumar D K'><span class='fa fa-linkedin'></span></a>
             <a href='https://www.paypal.me/heykarthikwithu' target='_blank' title='Paypal | Karthik Kumar D K'><span class='fa fa-paypal'></span></a>
             <a href='https://www.youtube.com/c/karthikkumardk' target='_blank' title='Youtube | Karthik Kumar D K'><span class='fa fa-youtube'></span></a>
-            <a href='/common-people/index.html' target='_blank' title='Common People's Voice'><span class='fa fa-users'></span></a>
+            <!-- a href='/common-people/index.html' target='_blank' title='Common People's Voice'><span class='fa fa-users'></span></a -->
         </div>
         <div class='copyright'><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class='fa fa-heart-o' aria-hidden='true'></i> by Colorlib
